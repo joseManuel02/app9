3c0f92f45d8f7c614f1d70f7e97d6b960215fa05
FherCopal <38224705+FherCopal@users.noreply.github.com>
2019-06-03 08:30:06 -0500

    6

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ $('#pause').hide();
 initAudio($('#playlist li:first-child'));
 
 function initAudio(element){
-	var song = element.attr('song');
+	var song= element.attr('song');
 	var title = element.text();
 	var cover = element.attr('cover');
 	var artist = element.attr('artist');
 	
-	//Create a New Audio Object
-	audio = new Audio('media/'+ song);
+	//Create a New Audio
+	audio = new Audio('media/' +song);
 	
 	if(!audio.currentTime){
 		$('#duration').html('0.00');
@@ -32,47 +32,47 @@ function initAudio(element){
 
 //Play Button
 $('#play').click(function (){
-	audio.play ();
+	audio.play();
 	$('#play').hide();
 	$('#pause').show();
 	$('#duration').fadeIn(400);
 	showDuration();
 });
 
-//Pause Button
+//Pause Botton
 $('#pause').click(function (){
-	audio.pause ();
-	$('#pause').show();
-	$('#play').hide();
+	audio.pause();
+	$('#pause').hide();
+	$('#play').show();
 });
 
 //Stop Button
 $('#stop').click(function (){
 	audio.pause ();
-	audio.currentTime =0;
+	audio.currentTime = 0;
 	$('#pause').hide();
 	$('#play').show();
-	$('#duration').fadeOut(400);
+	$('#duration').fadeOut(400);	
 });
 
 //Next Button
 $('#next').click(function (){
 	audio.pause ();
-	var next=$('#playlist li.active').next();
-	if(next.length==0){
-		next=$('#playlist li:first-child');
+	var next =$('#playlist li.active').next();
+	if(next.length==0) {
+		next =$('#playlist li:first-child');
 	}
 	initAudio (next);
 	audio.play();
 	showDuration();
 });
 
-//PrevButton
+//Prev Button
 $('#prev').click(function (){
-	audio.pause();
-	var prev=$('#palylist li.active').prev();
-	if(prev.length ==0){
-		prev =$('#playlist li:last-child');
+	audio.pause ();
+	var prev =$('#playlist li.active').prev();
+	if(prev.length==0){
+		prev = $('#playlist li:last-child');
 	}
 	initAudio (prev);
 	audio.play();
@@ -80,8 +80,8 @@ $('#prev').click(function (){
 });
 
 //Playlist Song Click
-$('#playlist li').click(function () {
-	audio.pause();
+$('#playlist li').click(function (){
+	audio.pause ();
 	initAudio($(this));
 	$('#play').hide();
 	$('#pause').show();
@@ -90,28 +90,29 @@ $('#playlist li').click(function () {
 	showDuration();
 });
 
-//Volumen Control
+//Volume Control
 $('#volume').change(function (){
-	audio.volume =parseFloat(this.value/10);
+	audio.volume =parseFloat(this.value / 10);
 });
 
 //Time Duration
-function showDuration (){
+function showDuration(){
 	$(audio).bind('timeupdate', function(){
 		//Get hours and minutes
-		var s = parseInt(audio.currentTime % 60);
-		var m = parseInt((audio.currentTime/60)% 60);
+		var s =parseInt(audio.currentTime % 60);
+		var m =parseInt((audio.currentTime / 60)%60);
 		//Add 0 if seconds less than 10
 		if(s<10){
-			s='0' + s;
+			s='0'+s;
 		}
-		$('#duration').html(m + '.' + s);
+		$('#duration').html(m+'.'+s);
 		var value=0;
-		if(audio.currentTime>0){
-			value = Math.floor((100/audio.duration)*audio.currentTime);
+		if(audio.currentTime > 0){
+			value =Math.floor((100/audio.duration)*audio.currentTime);
 		}
 		$('#progress').css('width',value+'%');
 	});
 }
+	
 	
 	
